0c721104f79e3cb07f8b5b5ce8b6e5777d40f59f
Geympadek <andreltsov08@outlook.com>
2024-11-18 20:39:16 +0400

    Idk

--- ozon.py
+++ ozon.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium_stealth import stealth
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 from bs4 import BeautifulSoup, NavigableString
 import re
 import asyncio
+
+import os
 
 def init_webdriver():
     options = Options()
@@ -23,20 +26,12 @@ def init_webdriver():
     # prevents browser from playing audio
     options.add_argument("--mute-audio")
     prefs = {
-        # prevents browser from loading images
-        "profile.managed_default_content_settings.images": 2,
         # shares location with every website
         "profile.default_content_setting_values.geolocation": 1
     }
     options.add_experimental_option("prefs", prefs)
 
     driver = webdriver.Chrome(options=options)
-
-    driver.execute_cdp_cmd("Emulation.setGeolocationOverride", {
-        "latitude": 56.91439917863945,
-        "longitude": 53.316618733502466,
-        "accuracy": 0  # Higher accuracy (in meters)
-    })
 
     stealth(driver,
             platform="Win32",
@@ -47,8 +42,31 @@ def init_webdriver():
 
 driver = init_webdriver()
 
-async def load_html(id: int):
-    driver.get(f"https://ozon.ru/product/{id}")
+def set_location(loc: tuple[int, int]):
+    driver.execute_cdp_cmd("Emulation.setGeolocationOverride", {
+        "latitude": loc[0],
+        "longitude": loc[1],
+        "accuracy": 0  # Higher accuracy (in meters)
+    })
+
+def enable_cdp_blocking():
+    # Enable Chrome DevTools Protocol (CDP) for blocking resources
+    driver.execute_cdp_cmd("Network.enable", {})
+    
+    # Set request interception patterns for types you want to block
+    driver.execute_cdp_cmd(
+        "Network.setBlockedURLs", {
+            "urls": [
+                "*.jpg", "*.jpeg", "*.png", "*.gif", "*.svg", "*.webp",  # Images
+                "*.css",  # Stylesheets
+                "*.mp4", "*.webm", "*.avi", "*.mov",  # Media
+                "*.ttf", "*.woff", "*.woff2", "*.otf"  # Fonts
+            ]
+        }
+    )
+
+async def load_html(link: str):
+    driver.get(link)
     
     await wait_antibot()
 
@@ -91,14 +109,74 @@ def get_price(html: BeautifulSoup):
 
     return price_from_spans(price_spans)
 
-async def load_info(id: int):
-    html = await load_html(id)
+async def wait_location():
+    '''
+    Finds a button, responsible for user's location
+    returns btn if found, returns None, if user
+    '''
+    while True:
+        try:
+            return driver.find_element(By.XPATH, "//button[contains(., 'Сменить')]")
+        except: pass
+        try:
+            driver.find_element(By.XPATH, "//button[contains(., 'Не сейчас')]")
+            return None
+        except: pass
+        await asyncio.sleep(0.1)
+
+async def wait_location_change():
+    while True:
+        try:
+            return driver.find_element(By.XPATH, "//h1[contains(., 'доставк')]")
+        except: pass
+        await asyncio.sleep(0.1)
+
+async def load_info(id: int, location: tuple[float, float] | None = None):
+    if location:
+        set_location(location)
+        print("Set location to ", location)
+    
+    # driver.delete_all_cookies()
+    print('Delete all cookies')
+
+    enable_cdp_blocking()
+
+    html = await load_html(f"https://ozon.ru/product/{id}")
+    print("load the page")
 
     name = get_name(html)
     if not name:
         return None
+
+    if location:
+        print("waiting for the button to appear")
+        change_btn = await wait_location()
+        if change_btn:
+            print("Emit click")
+            change_btn.click()
+
+            print("waiting for the location to change")
+            await wait_location_change()
+        else:
+            print("Location doesn't need to be changed")
+
     price = get_price(html)
     if not price:
         return None
 
     return {"name": name, "price": price * 100}
+
+# async def main():
+    
+
+#     article = 27524240
+
+#     enable_cdp_blocking()
+#     # preload for the future uses
+#     await load_html("https://www.ozon.ru/")
+
+#     # print(await load_info(article, (56.85423623447262, 53.242487506387455)))
+#     print(await load_info(article))
+#     await asyncio.sleep(120)
+
+# asyncio.run(main())
